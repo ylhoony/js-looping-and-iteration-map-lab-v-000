5990d0a59471ffa7a806952cfa736daa973e1893
Hoon Lee <yl.hoony@gmail.com>
2017-10-04 17:44:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file.
+
+
+function lowerCaseDrivers(collection) {
+  const newCollection = collection.map(function(driver) {
+    return driver.toLowerCase();
+  });
+  return newCollection;
+}
+
+function nameToAttributes(collection) {
+  const newCollection = [];
+  collection.map(function(driver) {
+    const nameArr = driver.split(' ');
+    const nameObject = {
+      firstName: nameArr[0],
+      lastName: nameArr[1]
+    }
+    newCollection.push(nameObject);
+  });
+  return newCollection;
+}
+
+function attributesToPhrase(collection) {
+  const newCollection = [];
+  collection.map(function(obj) {
+    newCollection.push(`${obj.name} is from ${obj.hometown}`);
+  });
+  return newCollection;
+}
